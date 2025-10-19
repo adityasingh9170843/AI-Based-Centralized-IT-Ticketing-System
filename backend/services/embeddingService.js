729846271f86300d10674c84555366130a96b029
Aditya Singh <adityaa9170843@gmail.com>
2025-10-19 12:21:56 +0530

    feat: add geminiService for ticket analysis with Google GenAI integration

--- backend/services/embeddingService.js
+++ backend/services/embeddingService.js
@@ -1,6 +1,7 @@
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { TaskType } from "@google/generative-ai";
-
+import dotenv from "dotenv";
+dotenv.config({quiet: true});
 
 const embeddings = new GoogleGenerativeAIEmbeddings({
   model: "text-embedding-004", 
---
+++ backend/services/geminiService.js
@@ -0,0 +1,43 @@
+import { GoogleGenAI } from "@google/genai";
+import dotenv from "dotenv";
+dotenv.config({ quiet: true });
+
+const ai = new GoogleGenAI({
+  apiKey: process.env.GOOGLE_GENAI_API_KEY,
+});
+
+export const analyzeTicket = async (ticketText) => {
+  try {
+    const SystemPrompt = `
+        You are an IT helpdesk assistant. Analyze the ticket and return ONLY valid JSON (no explanation).
+        Fields:
+        {
+            "department": "Network|Hardware|Software|Database|Security|General",
+            "priority": "Low|Medium|High",
+            "summary": "short summary"
+        }
+        Ticket:
+            """${ticketText}"""
+        `;
+
+    const response = await ai.models.generateContent({
+      model: "gemini-2.5-flash",
+      prompt: SystemPrompt,
+      temperature: 0.5,
+      maxOutputTokens: 100,
+      topP: 0.5,
+      topK: 0.5,
+    });
+
+    const result = JSON.parse(response.text);
+
+    return result;
+  } catch (error) {
+    console.log(error);
+    return JSON.stringify({
+      department: "General",
+      priority: "Medium",
+      summary: "",
+    });
+  }
+};
