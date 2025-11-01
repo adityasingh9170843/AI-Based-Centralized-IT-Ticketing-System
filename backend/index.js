a64cfdc2871714ca8ba458362bab2b523e4a4fbd
Aditya Singh <adityaa9170843@gmail.com>
2025-11-01 15:42:29 +0530

    integrate Tailwind CSS and Radix UI components
    - Updated package.json to include Tailwind CSS, Radix UI, and other dependencies.
    - Removed App.css styles and migrated to Tailwind CSS for styling.
    - Simplified App.jsx by removing unused code and components.
    - Configured Vite to use Tailwind CSS and set up path aliases for easier imports.
    - Added a new button component using Radix UI and class variance authority for styling.
    - Created utility functions for class name management with clsx and tailwind-merge.
    - Introduced components.json and jsconfig.json for component configuration and path mapping.

--- backend/index.js
+++ backend/index.js
@@ -1,33 +1,38 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import cors from 'cors';
-import { dbConnect } from './utils/dbConnect.js';
-import ticketRoutes from './routes/ticketRoutes.js';
-import analyticRoutes from './routes/analyticRoutes.js';
-import aiRoutes from './routes/aiRoutes.js';
-import departmentRoutes from './routes/departmentRoutes.js';
-import authRoutes from './routes/authRoute.js';
-import engineerRoutes from './routes/engineerRoutes.js';
-import './services/emailListener.js';
-
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import { dbConnect } from "./utils/dbConnect.js";
+import ticketRoutes from "./routes/ticketRoutes.js";
+import analyticRoutes from "./routes/analyticRoutes.js";
+import aiRoutes from "./routes/aiRoutes.js";
+import departmentRoutes from "./routes/departmentRoutes.js";
+import authRoutes from "./routes/authRoute.js";
+import engineerRoutes from "./routes/engineerRoutes.js";
+import "./services/emailListener.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(cookieParser());
 
-dotenv.config({quiet: true});
+dotenv.config({ quiet: true });
+
+app.use("/api/tickets", ticketRoutes);
+app.use("/api/departments", departmentRoutes);
+app.use("/api/engineers", engineerRoutes);
+app.use("/api/ai", aiRoutes);
+app.use("/api/analytic", analyticRoutes);
+app.use("/api/auth", authRoutes);
 
 
-app.use("/api/tickets",ticketRoutes);
-app.use("/api/departments",departmentRoutes);
-app.use("/api/engineers",engineerRoutes);
-app.use("/api/ai",aiRoutes);
-app.use("/api/analytic",analyticRoutes);
-app.use("/api/auth",authRoutes)
+
 app.listen(process.env.PORT, () => {
-    dbConnect();
-    console.log("Server is running on port " + process.env.PORT);
+  dbConnect();
+  console.log("Server is running on port " + process.env.PORT);
 });
