62e55e46f8ef5dbfe2360960aa3b2a0449396618
Aditya Singh <adityaa9170843@gmail.com>
2025-11-01 11:33:04 +0530

    Add user model with schema for user data management

---
+++ backend/models/userModel.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role:{
+        type:String,
+        enum: ["user", "admin","engineer"],
+        default: "user"
+    }
+})
